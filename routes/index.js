cc7106cf89d10b3c00eaebc1450ec4ad05993f55
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-03-06 19:51:33 +0100

    added auth

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth');
 
 /* GET / */
 router.get('/', (req, res, next) => {
@@ -9,5 +10,6 @@ router.get('/', (req, res, next) => {
 router.use('/album', require('./album'));
 router.use('/photo', require('./photo'));
 router.use('/user', require('./user'));
+router.use('/profile', auth.basic, require('./profile'));
 
 module.exports = router;
