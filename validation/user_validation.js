03a0eff7e1e28c9d4f818f7c9782a5336d88e3dd
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-03-06 14:00:51 +0100

    added custom validation

--- validation/user_validation.js
+++ validation/user_validation.js
@@ -9,7 +9,14 @@
   * Create user validation rules
   */
  const createRules = [
-    body('email').exists().isLength({min:6}),
+    body('email').exists().isLength({min:6}).custom(async value => {
+		const user = await new models.User({ email: value }).fetch({ require: false });
+		if (user) {
+			return Promise.reject("Email already exists.");
+		}
+
+		return Promise.resolve();
+	}),
     body('password').exists().isLength({min:8}),
     body('first_name').exists().isLength({min:2}),
     body('last_name').exists().isLength({min:2}),
