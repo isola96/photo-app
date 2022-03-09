8c13791583899f3967a0d90e36ce66de7e57afd2
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-03-09 15:29:40 +0100

    added working photocontrollers

--- controllers/photo_controller.js
+++ controllers/photo_controller.js
@@ -7,123 +7,154 @@ const { matchedData, validationResult } = require('express-validator');
 const models = require('../models');
 
 /**
- * Get all photos
+ * Get a users photos
  *
  * GET /
  */
-const index = async (req, res) => {
-	const all_photos = await models.Photo.fetchAll();
+ const getPhotos = async (req, res) => {
+    const usersPhotos = await new models.Photo().where({ 'user_id': req.user.id }).fetchAll({ columns: ['id', 'title', 'url', 'comment'] });
 
-	res.send({
-		status: 'success',
-		data: {
-			photos: all_photos,
-		}
-	});
-}
+    try {
+        res.status(200).send({
+            status: 'success',
+            data: usersPhotos
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            status: 'error',
+            message: 'Exception thrown in database when loading photos'
+        });
+        throw error;
+    }
+};
 
 /**
- * Get a specific photo
+ * Get a users specific photo
  *
- * GET /:photoId
+ * GET /photoId
  */
 const show = async (req, res) => {
-	const photo = await new models.Photo({ id: req.params.photoId })
-		.fetch();
+    await req.user.load('photos');
 
-	res.send({
-		status: 'success',
-		data: {
-			photo,
-		}
-	});
+    const onePhoto = await new models.Photo({ id: req.params.photoId });
+
+    const photoRelation = req.user.related('photos');
+
+    const foundPhoto = photoRelation.find(photo => photo.id == onePhoto.id);
+
+    if(!foundPhoto) {
+        return res.status(404).send({
+            status: 'fail',
+            data: 'An photo with that id could not be found in your list'
+        });
+    }
+
+    try {
+        res.send({
+            status: 'success',
+            data: foundPhoto
+        })
+    } catch (error) {
+        res.status(500).send({
+			status: 'error',
+			message: "Exception thrown when attempting to add photo",
+		});
+		throw error;
+    }
+    
 }
+
 
 /**
  * Store a new photo
  *
  * POST /
  */
-const store = async (req, res) => {
-	// check for any validation errors
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(422).send({ status: 'fail', data: errors.array() });
-	}
+ const store = async (req, res) => {
+    const errors = validationResult(req);
 
-	// get only the validated data from the request
-	const validData = matchedData(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).send({ status: 'fail', data: errors.array() })
+    }
 
-	try {
-		const photo = await new models.Photo(validData).save();
-		debug("Created new photo successfully: %O", photo);
+    const validData = matchedData(req);
 
-		res.send({
-			status: 'success',
-			data: {
-				photo,
-			}
-		});
+    validData.user_id = req.user.id;
 
-	} catch (error) {
-		res.status(500).send({
-			status: 'error',
-			message: 'Exception thrown in database when creating a new photo.',
-		});
-		throw error;
-	}
-}
+    try {
+        const photo = await new models.Photo(validData).save();
+
+        debug('Created new photo successfully: %O', photo);
+
+        res.status(200).send({
+            status: 'success',
+            data: photo
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            status: 'error',
+            message: 'Exception thrown in database when creating a new photo.'
+        });
+        throw error;
+    }
+};
 
 /**
- * Update a specific photo
+ * Update a users specific photo
  *
  * PUT /:photoId
  */
 const update = async (req, res) => {
-	const photoId = req.params.photoId;
+    await req.user.load('photos');
 
-	// make sure photo exists
-	const photo = await new models.Photo({ id: photoId }).fetch({ require: false });
-	if (!photo) {
-		debug("Photo to update was not found. %o", { id: photoId });
-		res.status(404).send({
-			status: 'fail',
-			data: 'Photo Not Found',
-		});
-		return;
-	}
+    const onePhoto = await new models.Photo({ id: req.params.photoId });
 
-	// check for any validation errors
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(422).send({ status: 'fail', data: errors.array() });
-	}
+    const photoRelation = req.user.related('photos');
 
-	// get only the validated data from the request
-	const validData = matchedData(req);
+    const foundPhoto = photoRelation.find(album => album.id == onePhoto.id);
 
-	try {
-		const updatedPhoto = await photo.save(validData);
-		debug("Updated photo successfully: %O", updatedPhoto);
+    if(!foundPhoto) {
+        return res.status(404).send({
+            status: 'fail',
+            data: 'Photo with that ID could not be found in your list.'
+        });
+    }
 
-		res.send({
-			status: 'success',
-			data: {
-				photo,
-			},
-		});
 
-	} catch (error) {
-		res.status(500).send({
-			status: 'error',
-			message: 'Exception thrown in database when updating a new photo.',
-		});
-		throw error;
-	}
-}
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(422).send({ 
+            status: 'fail', 
+            data: errors.array() 
+        })
+    }
+
+    const validData = matchedData(req);
+
+    try {
+        const updatedPhoto = await foundPhoto.save(validData);
+        
+        debug('Updated photo successfully: %O', updatedPhoto);
+
+        res.status(200).send({
+            status: 'success',
+            data: updatedPhoto
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            status: 'error',
+            message: 'Exception thrown in database when updating the photo.'
+        });
+        throw error;
+    }
+};
 
 module.exports = {
-	index,
+	getPhotos,
 	show,
 	store,
 	update,
