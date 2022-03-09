f20b5fa7321ae6003e44ef37553ea583d26597ed
Johanna Jönsson <johannajonsson96@hotmail.com>
2022-03-09 15:09:00 +0100

    removed destroy because not doing that and changed text to make sense

--- routes/album.js
+++ routes/album.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const albumController = require('../controllers/album_controller');
 const albumValidationRules = require('../validation/album_validation');
 
-/* Get all albums */
-router.get('/', albumController.index);
+/* Get users albums */
+router.get('/', albumController.getAlbums);
 
-/* Get a specific album */
-router.get('/:albumId', albumController.show);
+/* Get a users specific album */
+router.get('/:albumId', albumController.getOneAlbum);
 
 /* Store a new album */
 router.post('/', albumValidationRules.createRules, albumController.store);
 
-/* Update a specific album */
+/* Update a users specific album */
 router.put('/:albumId', albumValidationRules.updateRules,albumController.update);
+
+/* Add photo to an album */
+router.post('/:albumId/photos', albumValidationRules.addPhotoToAlbum, albumController.addPhoto);
 
 module.exports = router;
--- routes/photo.js
+++ routes/photo.js
@@ -3,21 +3,16 @@ const router = express.Router();
 const photoController = require('../controllers/photo_controller');
 const photoValidationRules = require('../validation/photo_validation');
 
-/* Get all photos */
-router.get('/', photoController.index);
+/* Get a users photos */
+router.get('/', photoController.getPhotos);
 
-/* Get a specific photo */
+/* Get a users specific photo */
 router.get('/:photoId', photoController.show);
 
 /* Store a new photo */
 router.post('/', photoValidationRules.createRules, photoController.store);
 
-/* Update a specific photo */
+/* Update a users specific photo */
 router.put('/:photoId', photoValidationRules.updateRules, photoController.update);
-
-
-/* Destroy a specific photo 
-router.delete('/:photoId', photoController.destroy);
-*/
 
 module.exports = router;
